602e6c336e388fddcbf7233c606193bea29d6757
Isabella Pagnoncelli <isabella.pagnoncelli@hotmail.com>
2020-03-25 20:29:35 -0300

    Motoristas.dat parcialmente adicionado, App reconfigurado e Motoristas.java feito

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /*
@@ -17,11 +18,16 @@ Para executar: java -cp .;.\commons-csv-1.7.jar App.java
 */
 public class App {
     private static final String SAMPLE_CSV_FILE_PATH = "veiculos.dat";
-
+    private static final String SAMPLE_CSV_FILE_PATH2 = "motorista.dat";
+    public enum FormaPagamento {DINHEIRO,CARTAO,TODAS;
+}
+    
     public static void main(String[] args) throws IOException {
         try (
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+            Reader reader2 = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH2));
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+            CSVParser csvParser2 = new CSVParser(reader2, CSVFormat.DEFAULT);
         ) {
             for (CSVRecord csvRecord : csvParser) {
                 // Accessing Values by Column Index
@@ -36,6 +42,22 @@ public class App {
                 System.out.println("Marca : " + marca);
                 System.out.println("Cor : " + cor);
                 System.out.println("Categoria : " + categoria);
+                System.out.println("---------------\n\n");
+            }
+
+            for (CSVRecord csvRecord : csvParser2){
+                String cpf = csvRecord.get(0);
+                String nome = csvRecord.get(1);
+                //Veiculo veiculo = csvRecord.get(2);
+                String FormaPagamento = csvRecord.get(3);
+
+
+                System.out.println("Record No - " + csvRecord.getRecordNumber());
+                System.out.println("---------------");
+                System.out.println("Cpf : " + cpf);
+                System.out.println("Nome : " + nome);
+                //System.out.println("Veiculo : " + veiculo);
+                System.out.println("Forma de Pagamento : " + FormaPagamento);
                 System.out.println("---------------\n\n");
             }
         }
--- Motorista.java
+++ Motorista.java
@@ -10,4 +10,23 @@ public class Motorista{
 String cpf;
 String nome;
 Veiculo veiculo;
+FormaPagamento formaPagamento;
+public enum FormaPagamento {DINHEIRO,CARTAO,TODAS;}
+
+public String getCpf() {
+    return cpf;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public Veiculo getVeiculo() {
+    return veiculo;
+}
+
+public FormaPagamento getFormaPagamento() {
+    return formaPagamento;
+}
+
 } 
