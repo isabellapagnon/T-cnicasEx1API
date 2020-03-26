cdedaf36a27a94673129431fc4aefa7691474918
Giulia Ferraro Borba <giulia.borba@acad.pucrs.br>
2020-03-25 21:14:56 -0300

    Adicionado a classe Veículo completa

--- Veiculo.java
+++ Veiculo.java
@@ -5,7 +5,37 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.lang.*;
 
-public class Veiculo{
+public class Veiculo {
+
+    public enum CategoriaVeiculo { SIMPLES, NORMAL, LUXO };
+    String placa;
+    String marca;
+    String cor;
+    CategoriaVeiculo categoria;
+
+    public Veiculo(String placa, String marca, String cor, CategoriaVeiculo categoria) {
+        this.placa = placa;
+        this.marca = marca;
+        this.cor = cor;
+        this.categoria = categoria;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public CategoriaVeiculo getCategoria() {
+        return categoria;
+    }
 
 }
